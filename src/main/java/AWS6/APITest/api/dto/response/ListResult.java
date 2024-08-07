dd52640fa5748bc64c632cb2dceacf2768619ce8
gytjd <hys339631@gmail.com>
2024-08-07 20:04:31 +0900

    [Feat]: ListResult 개발
    ListResult.java : 다중 객체 값 Json 변환

---
+++ src/main/java/AWS6/APITest/api/dto/response/ListResult.java
@@ -0,0 +1,14 @@
+package AWS6.APITest.api.dto.response;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class ListResult<T> extends CommonResult {
+    private List<T> list;
+
+    public ListResult(List<T> list) {
+        this.list = list;
+    }
+}
